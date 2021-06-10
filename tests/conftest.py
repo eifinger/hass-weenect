5fa80e1e94cb11ac0587a48b55c2d720c744961e
Kevin Eifinger <k.eifinger@googlemail.com>
2021-06-10 14:11:06 +0200

    chore: Use enable_custom_integrations fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,15 @@ from unittest.mock import patch
 
 import pytest
 
-pytest_plugins = "pytest_homeassistant_custom_component"
+pytest_plugins = "pytest_homeassistant_custom_component"  # pylint: disable=invalid-name
+
+
+# This fixture enables loading custom integrations in all tests.
+# Remove to enable selective use of this fixture
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integration loading."""
+    yield
 
 
 # This fixture is used to prevent HomeAssistant from attempting to create and dismiss persistent
