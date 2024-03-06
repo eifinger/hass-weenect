08b740d39e47a2bafab7cfda3f7c40401909ee49
Andreas Brett <github@abrett.de>
2024-03-06 15:36:15 +0100

    timezone not needed anymore

--- custom_components/weenect/sensor.py
+++ custom_components/weenect/sensor.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import List
 
 from homeassistant.components.sensor import (
